dc90fc2732d399f7fc29d59526b4141692ddae1e
NadavZ101 <nadavzivyon@gmail.com>
2024-03-13 12:52:00 +0200

    Set up MailIndex with loadMail function and render the data from service

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,5 +1,37 @@
+const { useState, useEffect } = React
+
+import { mailService } from "./../services/mail.service.js"
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [mails, setMails] = useState(null)
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    function loadMails() {
+        mailService.query()
+            .then(mails => {
+                setMails(mails)
+            })
+    }
+
+    if (!mails) return <div>Loading emails...</div>
+    return <section className="mail-index">
+
+        <ul>
+            {
+                mails.map(mail => <li key={mail.id}>
+                    {mail.from}
+                    {mail.subject}
+                    {mail.sentAt}
+                </li>)
+            }
+        </ul>
+
+        <div>mail app</div>
+    </section>
+
 }
 
