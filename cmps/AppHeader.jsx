a774757bc180fb1c8de6de1eb591d6130b42f1ef
NadavZ101 <nadavzivyon@gmail.com>
2024-03-16 21:19:49 +0200

    Style the Home Page & about Page

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,26 @@
 const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
 
-    return <header className="app-header">
+    const [menu, setMenu] = useState('menu-close')
+
+    function toggleMenu() {
+        setMenu(menu === 'menu-close' ? 'menu-open' : 'menu-close')
+    }
+
+    return <header className="app-header flex">
         <Link to="/">
-            <h3>LOGO!</h3>
+            {/* <img src="/assets/img/appsus1.png" alt="logo" /> */}
         </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
+        {menu === 'menu-open' && <div className="backdrop" onClick={() => setMenu('menu-close')}></div>}
+        <nav className={`header-nav-btns ${menu}`}>
+            <NavLink className="header-nav-btns-home" to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
         </nav>
-    </header>
+
+        <button class="toggle-menu-btn" onClick={() => toggleMenu('menu-open')} type="button">☰</button>
+    </header >
 }
--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,20 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1>About The Team</h1>
+        <article>
+            Welcome to AppSus, an innovative platform where convenience and efficiency meet. AppSus was brought to life by two visionary developers, Haim and Nadav, who shared a common goal: to create a unified solution for email management and note-keeping that caters to the diverse needs of modern users.
+            <br></br>
+            With backgrounds in both software development and user experience design, Haim and Nadav embarked on a journey to develop an application that not only addresses the functional requirements of its users but also provides a seamless and intuitive experience. Their collaboration resulted in the creation of AppSus, a testament to their dedication and expertise in the field of digital solutions.
+            <br></br>
+
+            AppSus stands out as a reflection of Haim and Nadav's innovative approach to problem-solving and their commitment to excellence. By integrating mail services with a versatile note-keeping system, they have crafted an all-in-one platform that simplifies the lives of its users, enabling them to manage their communications and ideas with unprecedented ease.
+            <br></br>
+
+            Beyond the technical achievements, AppSus is a product of Haim and Nadav's passion for creating meaningful and impactful software. They continue to work tirelessly, constantly seeking new ways to enhance AppSus and ensure that it remains at the forefront of digital convenience.
+            <br></br>
+
+            We invite you to explore AppSus and experience the thoughtful design and functionality that Haim and Nadav have infused into this application. Join us on this journey of innovation and discover how AppSus can transform the way you manage your emails and notes.
+        </article>
     </section>
 }
