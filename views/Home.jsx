b93fa819df81e36fb3cf226a1b889e0bb6fcf40a
NadavZ101 <nadavzivyon@gmail.com>
2024-03-17 10:33:13 +0200

    Home layout fix

--- views/Home.jsx
+++ views/Home.jsx
@@ -7,18 +7,17 @@ export function Home() {
             <img style={{ width: "20%" }} className="home-img" src="assets/img/appsus1.png" alt="logo" />
         </div>
 
-        <article>AppSus is a dynamic and innovative application designed to streamline your communication and organization needs in one convenient platform. With its seamless integration of mail services and note-keeping functionalities, AppSus empowers users to manage their emails and notes effortlessly, all under one roof.</article>
-        <br></br>
+        <p>AppSus is a dynamic and innovative application designed to streamline your communication and organization needs in one convenient platform. With its seamless integration of mail services and note-keeping functionalities, AppSus empowers users to manage their emails and notes effortlessly, all under one roof.</p>
 
-        <article>
+        <p>
             Complementing the mail services, AppSus's note keeper is a versatile tool for jotting down ideas, making lists, and keeping important information at your fingertips. Whether you need to remember a critical task, capture a fleeting thought, or plan your day, the note keeper is designed to help you keep everything organized in a simple and efficient manner.
-        </article>
-        <article>
+        </p>
 
+        <p>
             <br></br>
             With AppSus, users enjoy the convenience of having both essential services in one application, eliminating the need to switch between different apps or tabs. This integrated approach not only saves time but also enhances productivity and organization.
 
             Whether you're a busy professional, a student, or anyone in between, AppSus is your go-to solution for managing your communications and notes effectively. Experience the future of convenience with AppSus, where emails and notes come together seamlessly.
-        </article>
+        </p>
     </section >
 }
